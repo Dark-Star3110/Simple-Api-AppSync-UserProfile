fa5a93e7b5e48a9e4d63ccf9672b144e6f596807
Vandt <dangvan31102001@gmail.com>
2023-05-28 10:03:53 +0700

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-# Welcome to your CDK TypeScript project
+# Simple Secure AWS AppSync with API Keys and DynamoDB
+
+This repo walks through the steps needed to get setup an AppSync API that is protected with an API Key.
+
+```typescript
+const api = new GraphqlApi(this, "User API", {
+  name: "User API",
+  schema: SchemaFile.fromAsset(path.join(__dirname, "graphql/schema.graphql")),
+  authorizationConfig: {
+    defaultAuthorization: {
+      authorizationType: AuthorizationType.API_KEY,
+      apiKeyConfig: {
+        description: "public scan for Users",
+        expires: Expiration.after(Duration.days(30)),
+        name: "API Key for demo",
+      },
+    },
+  },
+});
+```
+
+![architecture diagram](./readmeImg/archDiagram.png)
+
+## Project Overview
+
+The deployed project is meant to work with a frontend (e.g NextJS), thereby creating a fullstack application.
+
+![architecture diagram](./readmeImg/infra.png)
+
+To deploy an API for client usage, we can use the following command.
+
+- `cdk deploy --outputs-file ./output.json`
+  => The output.json file is generated containing the necessary configuration information for the client
+  On the frontend, use of the AWS Amplify JS libraries are used to connect our frontend to our backend by means of the Amplify.configure method (sample data configs are used):
+
+```typescript
+Amplify.configure({
+  aws_project_region: "ap-northeast-1",
+  aws_appsync_graphqlEndpoint: "your aws_appsync_graphqlEndpoint",
+  aws_appsync_region: "ap-northeast-1",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "your aws_appsync_apiKey",
+});
+```
 
 This is a blank project for CDK development with TypeScript.
 
@@ -6,9 +49,9 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,16 +1,90 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { CfnOutput, Duration, Expiration, RemovalPolicy } from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import {
+  GraphqlApi,
+  SchemaFile,
+  AuthorizationType,
+  FieldLogLevel,
+  MappingTemplate,
+} from "@aws-cdk/aws-appsync-alpha";
+import * as path from "path";
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB
+    const userTable = new Table(this, "User Table", {
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "userId", type: AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // AppSync
+    const api = new GraphqlApi(this, "User API", {
+      name: "User API",
+      schema: SchemaFile.fromAsset(
+        path.join(__dirname, "graphql/schema.graphql")
+      ),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            description: "public scan for Users",
+            expires: Expiration.after(Duration.days(30)),
+            name: "API Key for demo",
+          },
+        },
+      },
+      logConfig: {
+        fieldLogLevel: FieldLogLevel.ALL,
+      },
+      xrayEnabled: true,
+    });
+
+    // VTL Resolve
+    const userTableDataSource = api.addDynamoDbDataSource(
+      "userDataSource",
+      userTable
+    );
+    // mutation
+    userTableDataSource.createResolver("mutationCreateUser", {
+      typeName: "Mutation",
+      fieldName: "createUser",
+      requestMappingTemplate: MappingTemplate.fromFile(
+        path.join(
+          __dirname,
+          "graphql/mappingTemplates/Mutation.createUser.req.vtl"
+        )
+      ),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
+    });
+
+    // query
+    userTableDataSource.createResolver("queryListUsers", {
+      typeName: "Query",
+      fieldName: "listUsers",
+      requestMappingTemplate: MappingTemplate.fromFile(
+        path.join(__dirname, "graphql/mappingTemplates/Query.listUsers.req.vtl")
+      ),
+      responseMappingTemplate: MappingTemplate.fromFile(
+        path.join(__dirname, "graphql/mappingTemplates/Query.listUsers.res.vtl")
+      ),
+    });
+
+    // export configure for client
+    new CfnOutput(this, "GraphQLAPIID", {
+      value: api.apiId,
+    });
+
+    new CfnOutput(this, "GraphQLURL", {
+      value: api.graphqlUrl,
+    });
+
+    new CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
   }
 }
